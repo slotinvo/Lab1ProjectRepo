2d611ba56339751af484daca6a22d653761751de
slotinvo <slotin.vitya@mail.ru>
2022-04-28 15:53:06 +0300

    Изменил handler

--- MyLib/Task2.cs
+++ MyLib/Task2.cs
@@ -8,27 +8,40 @@ namespace MyLib
 {
     public class Countdown
     {
-        public event EventHandler<string>? handler;
-
-        public void ItHappens(int timeSleep)
+        public event EventHandler<string[]>? handler;
+        public string channelName;
+        public Countdown(string channelName)
         {
+            this.channelName = channelName;
+        }
+
+        public void Mailing(int timeSleep, string messege)
+        {
+            string[] args = new string[2];
+            args[0] = channelName;
+            args[1] = messege;
             Thread.Sleep(timeSleep);
-            Console.WriteLine("Отправка рассылки от Countdown!");
-            handler?.Invoke(this, "Привет!");
+            Console.WriteLine("Отправка рассылки от канала " + channelName);
+            handler?.Invoke(this, args);
         }
     }
     public class Subscriber
     {
         private readonly string name;
-        public Subscriber(Countdown cd, string name)
+        private readonly string[] subscriptions;
+        public Subscriber(Countdown cd, string name, string[] subscriptions)
         {
             cd.handler += DisplayMessage;
             this.name = name;
+            this.subscriptions = subscriptions;
         }
 
-        private void DisplayMessage(object? sender, string e)
+        private void DisplayMessage(object? sender, string[] args)
         {
-            Console.WriteLine($"Подписчик {name} реагирует на событие Countdown. Сообщение: {e}");
+            if (subscriptions.Contains(args[0]))
+            {
+                Console.WriteLine($"{name}! {args[1]}");
+            }
         }
     }
 }
